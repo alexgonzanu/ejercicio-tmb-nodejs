2e58e6225a86d39070a2970b61589ba6ff7d60ce
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-03-31 12:15:05 +0200

    si elige metro te devuelve opcion de 2 preguntas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 inquirer.prompt([
   {
@@ -8,12 +9,43 @@ inquirer.prompt([
     choices: [
       {
         name: "metro",
-        value: "metro"
+        value: "Metro"
       },
       {
         name: "bus",
-        value: "bus"
+        value: "Bus"
       }
     ]
-  }])
-  .then(resp => console.log(resp));
+  },
+  {
+    type: "checkbox",
+    name: "informacion extra",
+    choices: [
+      {
+        name: "coordenadas",
+        value: "Coordenadas"
+      },
+      {
+        name: "fecha-inaguracion",
+        value: "Fecha de inaguración"
+      }
+    ],
+    when: respuesta => respuesta.transporte === "Metro"
+  }
+])
+  .then(resp => {
+    /* switch (resp.transporte) {
+      case "Bus":
+        console.log(chalk.yellow("No tenemos información disponible sobre los buses"));
+        process.exit(0);
+        break;
+      case "Metro":
+        break;
+      default:
+        break;
+    } */
+    if (resp.transporte === "Bus") {
+      console.log(chalk.yellow("No tenemos información disponible sobre los buses"));
+      process.exit(0);
+    }
+  });
