84b920e1b9da4a765f520e18d870c860b5e34bcf
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-03-31 12:47:45 +0200

    puesto el array de preguntas en un fichero aparte

--- index.js
+++ index.js
@@ -1,60 +1,13 @@
 const inquirer = require("inquirer");
-const chalk = require("chalk");
 const { program } = require("commander");
+const chalk = require("chalk");
+const preguntas = require("./arrayPreguntas");
 
 program
   .option("-c --color", "Muestra colores")
   .option("a --abrev", "Abreviatura", false);
 
-inquirer.prompt([
-  {
-    type: "list",
-    name: "transporte",
-    message: "¿Qué tipo de transporte quiere consultar?",
-    choices: [
-      {
-        name: "metro",
-        value: "Metro"
-      },
-      {
-        name: "bus",
-        value: "Bus"
-      }
-    ]
-  },
-  {
-    type: "checkbox",
-    name: "informacion extra",
-    message: "¿Qué información extra quiere obtener de cada parada?",
-    choices: [
-      {
-        name: "coordenadas",
-        value: "Coordenadas"
-      },
-      {
-        name: "fecha-inaguracion",
-        value: "Fecha de inaguración"
-      }
-    ],
-    when: respuesta => respuesta.transporte === "Metro",
-  },
-  {
-    type: "confirm",
-    name: "info-errores",
-    message: "¿Quiere que le informemos de los errores?",
-    when: (respuesta) => {
-      if (respuesta.transporte === "Bus") {
-        console.log(chalk.yellow("No tenemos información disponible sobre los buses"));
-        process.exit(0);
-      }
-    }
-  },
-  {
-    type: "input",
-    name: "linia-escogida",
-    message: "¿Qué línea quiere consultar?"
-  }
-])
+inquirer.prompt(preguntas)
   .then(resp => {
     if (resp.transporte === "Bus") {
       console.log(chalk.yellow("No tenemos información disponible sobre los buses"));
